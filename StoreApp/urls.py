8225174dae3bddf41aac6e69d95d9900834dacc5
Luis Perez <luisperez07182@email.com>
2024-04-22 04:01:02 -0500

    added action, templates and urls for index

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', views.product_index, name='product_index'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Product
+
+def product_index(request):
+    products = Product.objects.all
+    return render(request, 'product/index.html', {'products': products})
+
+
 # Create your views here.
